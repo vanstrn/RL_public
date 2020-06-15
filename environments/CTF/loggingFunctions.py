2dadfe299d58dc9be6ac0c1a4d26d631cd334e86
vanstrn2 <nealeav2@illinois.edu>
2020-06-15 16:17:30 -0500

    Finished Modularizing SF and SF Hierarchy Training. Need to change all relevant config files. Issues with creating Q-map for large CTF games

--- environments/CTF/loggingFunctions.py
+++ environments/CTF/loggingFunctions.py
@@ -6,14 +6,18 @@ import matplotlib.image as mpimg
 import random
 
 class ValueTest_StackedStates(tf.keras.callbacks.Callback):
-    def __init__(self,superEpochs):
+    def __init__(self,superEpochs,env,network,imageDir=None,freq=100):
+        self.env = env
+        self.network=network[3]
+        self.imageDir=imageDir
+        self.freq = freq
         self.superEpochs = superEpochs
     def on_epoch_end(self,epoch, logs=None):
-        if self.superEpochs%settings["LogFreq"] == 0:
-            env.reset()
-            rewardMap = np.zeros((dFeatures[0],dFeatures[1]))
-            for i,j in itertools.product(range(dFeatures[0]),range(dFeatures[1])):
-                grid = env.ConstructSample([i,j])
+        if self.superEpochs%self.freq == 0:
+            s=self.env.reset()
+            rewardMap = np.zeros((s.shape[1],s.shape[2]))
+            for i,j in itertools.product(range(s.shape[1]),range(s.shape[2])):
+                grid = self.env.ConstructSample([i,j])
                 if grid is None: continue
                 tmp = np.concatenate([grid] * 4, axis=2)
                 [value] = SF4.predict(np.expand_dims(tmp,0))
@@ -21,84 +25,12 @@ class ValueTest_StackedStates(tf.keras.callbacks.Callback):
             fig=plt.figure(figsize=(5.5, 8))
             fig.add_subplot(2,1,1)
             plt.title("State")
-            imgplot = plt.imshow(env.get_obs_blue[:,:,2], vmin=-1, vmax=1)
+            imgplot = plt.imshow(self.env.get_obs_blue[:,:,2], vmin=-1, vmax=1)
             fig.add_subplot(2,1,2)
             plt.title("Value Prediction")
             imgplot = plt.imshow(rewardMap)
             fig.colorbar(imgplot)
-            plt.savefig(LOG_PATH+"/ValuePred"+str(self.superEpochs)+".png")
-            plt.close()
-
-class ImageGenerator_StackedStates(tf.keras.callbacks.Callback):
-    def __init__(self,env,network,imageDir=None,freq=100):
-        self.env = env
-        self.network=network[0]
-        self.imageDir = imageDir
-        self.freq = freq
-    def on_epoch_end(self,epoch, logs=None):
-        if epoch%self.freq == 0:
-            for i in range(5):
-                state = self.env.reset()
-                act = [random.randint(0,4)]
-                [state_new,reward] = SF1.predict([np.expand_dims(act,0),state])
-                fig=plt.figure(figsize=(16, 8))
-                fig.add_subplot(2,4,1)
-                plt.title("State Territory")
-                imgplot = plt.imshow(state[0,:,:,1], vmin=-2, vmax=2)
-                fig.add_subplot(2,4,2)
-                plt.title("State Flags")
-                imgplot = plt.imshow(state[0,:,:,2], vmin=-10, vmax=10)
-                fig.add_subplot(2,4,3)
-                plt.title("State Obstacles")
-                imgplot = plt.imshow(state[0,:,:,3], vmin=-2, vmax=2)
-                fig.add_subplot(2,4,4)
-                plt.title("State Agents")
-                imgplot = plt.imshow(state[0,:,:,4],vmin=-10, vmax=10)
-                fig.add_subplot(2,4,5)
-                plt.title("Predicted Next State Territory")
-                imgplot = plt.imshow(state_new[0,:,:,1],vmin=-2, vmax=2)
-                fig.add_subplot(2,4,6)
-                plt.title("Predicted Next State Flags")
-                imgplot = plt.imshow(state_new[0,:,:,2],vmin=-10, vmax=10)
-                fig.add_subplot(2,4,7)
-                plt.title("Predicted Next State Obstacles")
-                imgplot = plt.imshow(state_new[0,:,:,3],vmin=-2, vmax=2)
-                fig.add_subplot(2,4,8)
-                plt.title("Predicted Next State Agents")
-                imgplot = plt.imshow(state_new[0,:,:,4],vmin=-10, vmax=10)
-                if i == 0:
-                    plt.savefig(LOG_PATH+"/StatePredEpoch"+str(epoch)+".png")
-                else:
-                    plt.savefig(LOG_PATH+"/StatePred"+str(i)+".png")
-                plt.close()
-
-
-class RewardTest_StackedStates(tf.keras.callbacks.Callback):
-
-    def __init__(self,superEpochs,env,network,imageDir=None,freq=100):
-        self.env = env
-        self.network=network[0]
-        self.imageDir = imageDir
-        self.freq = freq
-    def on_epoch_end(self,epoch, logs=None):
-        if epoch%settings["LogFreq"] == 0:
-            env.reset()
-            rewardMap = np.zeros((dFeatures[0],dFeatures[1]))
-            for i,j in itertools.product(range(dFeatures[0]),range(dFeatures[1])):
-                grid = env.ConstructSample([i,j])
-                if grid is None: continue
-                tmp = np.concatenate([grid] * 4, axis=2)
-                [state_new,reward] = SF1.predict([np.stack([[1,1,1,1,1]]),np.expand_dims(tmp,0)])
-                rewardMap[i,j] = reward
-            fig=plt.figure(figsize=(5.5, 8))
-            fig.add_subplot(2,1,1)
-            plt.title("State")
-            imgplot = plt.imshow(env.get_obs_blue[:,:,2], vmin=-1, vmax=1)
-            fig.add_subplot(2,1,2)
-            plt.title("Reward Prediction Epoch "+str(epoch))
-            imgplot = plt.imshow(rewardMap)
-            fig.colorbar(imgplot)
-            plt.savefig(LOG_PATH+"/RewardPred"+str(epoch)+".png")
+            plt.savefig(self.imageDir+"/ValuePred"+str(self.superEpochs)+".png")
             plt.close()
 
 class ValueTest(tf.keras.callbacks.Callback):
@@ -129,7 +61,7 @@ class ValueTest(tf.keras.callbacks.Callback):
             plt.savefig(self.imageDir+"/ValuePred"+str(self.superEpochs)+".png")
             plt.close()
 
-class ImageGenerator(tf.keras.callbacks.Callback):
+class ImageGenerator_StackedStates(tf.keras.callbacks.Callback):
     def __init__(self,env,network,imageDir=None,freq=100):
         self.env = env
         self.network=network[0]
@@ -139,6 +71,81 @@ class ImageGenerator(tf.keras.callbacks.Callback):
         if epoch%self.freq == 0:
             state = self.env.reset()
             [state_new,reward] = self.network.predict([np.stack([[0,0,0,0,0]]),state])
+            fig=plt.figure(figsize=(16, 8))
+            fig.add_subplot(2,4,1)
+            plt.title("State Territory")
+            imgplot = plt.imshow(state[0,:,:,1], vmin=-2, vmax=2)
+            fig.add_subplot(2,4,2)
+            plt.title("State Flags")
+            imgplot = plt.imshow(state[0,:,:,2], vmin=-10, vmax=10)
+            fig.add_subplot(2,4,3)
+            plt.title("State Obstacles")
+            imgplot = plt.imshow(state[0,:,:,3], vmin=-2, vmax=2)
+            fig.add_subplot(2,4,4)
+            plt.title("State Agents")
+            imgplot = plt.imshow(state[0,:,:,4],vmin=-10, vmax=10)
+            fig.add_subplot(2,4,5)
+            plt.title("Predicted Next State Territory")
+            imgplot = plt.imshow(state_new[0,:,:,1],vmin=-2, vmax=2)
+            fig.add_subplot(2,4,6)
+            plt.title("Predicted Next State Flags")
+            imgplot = plt.imshow(state_new[0,:,:,2],vmin=-10, vmax=10)
+            fig.add_subplot(2,4,7)
+            plt.title("Predicted Next State Obstacles")
+            imgplot = plt.imshow(state_new[0,:,:,3],vmin=-2, vmax=2)
+            fig.add_subplot(2,4,8)
+            plt.title("Predicted Next State Agents")
+            imgplot = plt.imshow(state_new[0,:,:,4],vmin=-10, vmax=10)
+            plt.savefig(self.imageDir+"/StatePredEpoch"+str(epoch)+".png")
+
+class ImageGenerator_actions(tf.keras.callbacks.Callback):
+    def __init__(self,env,network,imageDir=None,freq=100):
+        self.env = env
+        self.network=network[0]
+        self.imageDir = imageDir
+        self.freq = freq
+    def on_epoch_end(self,epoch, logs=None):
+        if epoch%self.freq == 0:
+            state = self.env.reset()
+            [state_new,reward] = self.network.predict([np.stack([[0,0,0,0,0]]),state])
+            fig=plt.figure(figsize=(16, 8))
+            fig.add_subplot(2,4,1)
+            plt.title("State Territory")
+            imgplot = plt.imshow(state[0,:,:,1], vmin=-2, vmax=2)
+            fig.add_subplot(2,4,2)
+            plt.title("State Flags")
+            imgplot = plt.imshow(state[0,:,:,2], vmin=-10, vmax=10)
+            fig.add_subplot(2,4,3)
+            plt.title("State Obstacles")
+            imgplot = plt.imshow(state[0,:,:,3], vmin=-2, vmax=2)
+            fig.add_subplot(2,4,4)
+            plt.title("State Agents")
+            imgplot = plt.imshow(state[0,:,:,4],vmin=-10, vmax=10)
+            fig.add_subplot(2,4,5)
+            plt.title("Predicted Next State Territory")
+            imgplot = plt.imshow(state_new[0,:,:,1],vmin=-2, vmax=2)
+            fig.add_subplot(2,4,6)
+            plt.title("Predicted Next State Flags")
+            imgplot = plt.imshow(state_new[0,:,:,2],vmin=-10, vmax=10)
+            fig.add_subplot(2,4,7)
+            plt.title("Predicted Next State Obstacles")
+            imgplot = plt.imshow(state_new[0,:,:,3],vmin=-2, vmax=2)
+            fig.add_subplot(2,4,8)
+            plt.title("Predicted Next State Agents")
+            imgplot = plt.imshow(state_new[0,:,:,4],vmin=-10, vmax=10)
+            plt.savefig(self.imageDir+"/StatePredEpoch"+str(epoch)+".png")
+            plt.close()
+
+class ImageGenerator(tf.keras.callbacks.Callback):
+    def __init__(self,env,network,imageDir=None,freq=100):
+        self.env = env
+        self.network=network[0]
+        self.imageDir = imageDir
+        self.freq = freq
+    def on_epoch_end(self,epoch, logs=None):
+        if epoch%self.freq == 0:
+            state = self.env.reset()
+            [state_new,reward] = self.network.predict([state])
             fig=plt.figure(figsize=(16, 8))
             fig.add_subplot(2,4,1)
             plt.title("State Territory")
@@ -182,7 +189,62 @@ class RewardTest(tf.keras.callbacks.Callback):
             for i,j in itertools.product(range(s.shape[1]),range(s.shape[2])):
                 grid = self.env.ConstructSample([i,j])
                 if grid is None: continue
+                [state_new,reward] = self.network.predict(np.expand_dims(grid,0))
+                rewardMap[i,j] = reward
+            fig=plt.figure(figsize=(5.5, 8))
+            fig.add_subplot(2,1,1)
+            plt.title("State")
+            imgplot = plt.imshow(self.env.get_obs_blue[:,:,2], vmin=-1, vmax=1)
+            fig.add_subplot(2,1,2)
+            plt.title("Reward Prediction Epoch "+str(epoch))
+            imgplot = plt.imshow(rewardMap)
+            fig.colorbar(imgplot)
+            plt.savefig(self.imageDir+"/RewardPred"+str(epoch)+".png")
+            plt.close()
+
+class RewardTest_actions(tf.keras.callbacks.Callback):
+    def __init__(self,env,network,imageDir=None,freq=100):
+        self.env = env
+        self.network=network[0]
+        self.imageDir = imageDir
+        self.freq = freq
+
+    def on_epoch_end(self,epoch, logs=None):
+        if epoch%self.freq == 0:
+            s = self.env.reset()
+            rewardMap = np.zeros((s.shape[1],s.shape[2]))
+            for i,j in itertools.product(range(s.shape[1]),range(s.shape[2])):
+                grid = self.env.ConstructSample([i,j])
+                if grid is None: continue
                 [state_new,reward] = self.network.predict([np.stack([[0,0,0,0,0]]),np.expand_dims(grid,0)])
+                rewardMap[i,j] = reward
+            fig=plt.figure(figsize=(5.5, 8))
+            fig.add_subplot(2,1,1)
+            plt.title("State")
+            imgplot = plt.imshow(self.env.get_obs_blue[:,:,2], vmin=-1, vmax=1)
+            fig.add_subplot(2,1,2)
+            plt.title("Reward Prediction Epoch "+str(epoch))
+            imgplot = plt.imshow(rewardMap)
+            fig.colorbar(imgplot)
+            plt.savefig(self.imageDir+"/RewardPred"+str(epoch)+".png")
+            plt.close()
+
+class RewardTest_StackedStates(tf.keras.callbacks.Callback):
+
+    def __init__(self,env,network,imageDir=None,freq=100):
+        self.env = env
+        self.network=network[0]
+        self.imageDir = imageDir
+        self.freq = freq
+    def on_epoch_end(self,epoch, logs=None):
+        if epoch%self.freq == 0:
+            s = self.env.reset()
+            rewardMap = np.zeros((s.shape[1],s.shape[2]))
+            for i,j in itertools.product(range(s.shape[1]),range(s.shape[2])):
+                grid = self.env.ConstructSample([i,j])
+                if grid is None: continue
+                tmp = np.concatenate([grid] * 4, axis=2)
+                [state_new,reward] = self.network.predict([np.stack([[1,1,1,1,1]]),np.expand_dims(tmp,0)])
                 rewardMap[i,j] = reward
             fig=plt.figure(figsize=(5.5, 8))
             fig.add_subplot(2,1,1)
