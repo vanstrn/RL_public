bb4bc8fd6c55f5f3d3d08cbdbc513cdca5487785
vanstrn <33610524+vanstrn@users.noreply.github.com>
2020-07-14 22:44:04 -0400

    Adding SFH testing scripts and testplans.

---
+++ tp_CTF1v0_SFsH_exp.bash
@@ -0,0 +1,23 @@
+echo "Batch Number" $1
+if [ -z "$2" ]
+  then
+    echo "Device argument not supplied defaulting to CPU"
+    DEVICE=cpu
+  else
+    DEVICE=$2
+fi
+echo "Using device:" $DEVICE
+
+python SF_v2_action.py -f CTFP20_SFs.json -c '{"RunName":"CTF1v0_SFs_256_'$1'"}' -n '{"DefaultParams":{"SFSize":"256"}}' -p /$DEVICE:0
+
+python SingleExec_v4.py -f CTFP20_SFsH.json -c '{"RunName":"CTF1v0_SFsH_256_'$1'_1_PPO_HP","LoadName":"CTF1v0_SFs_256_'$1'","TotalSamples":256,"Selection":"Hull_pca","NumOptions":24,
+    "NetworkHPs":{"EntropyBeta":0.1,"LR Actor":0.00005,"LR Entropy":0.0000001,"BatchSize":256,"MinibatchSize":32,"CriticBeta":0.5,"Gamma":0.98,"lambda":0.98,"Epochs":1}}' -p /$DEVICE:0 &
+python SingleExec_v4.py -f CTFP20_SFsH.json -c '{"RunName":"CTF1v0_SFsH_256_'$1'_1_PPO_RS","LoadName":"CTF1v0_SFs_256_'$1'","TotalSamples":256,"Selection":"Random_sampling","NumOptions":24,
+    "NetworkHPs":{"EntropyBeta":0.1,"LR Actor":0.00005,"LR Entropy":0.0000001,"BatchSize":256,"MinibatchSize":32,"CriticBeta":0.5,"Gamma":0.98,"lambda":0.98,"Epochs":1}}' -p /$DEVICE:0 &
+python SingleExec_v4.py -f CTFP20_SFsH.json -c '{"RunName":"CTF1v0_SFsH_256_'$1'_1_PPO_FS","LoadName":"CTF1v0_SFs_256_'$1'","TotalSamples":256,"Selection":"First","NumOptions":24,
+    "NetworkHPs":{"EntropyBeta":0.1,"LR Actor":0.00005,"LR Entropy":0.0000001,"BatchSize":256,"MinibatchSize":32,"CriticBeta":0.5,"Gamma":0.98,"lambda":0.98,"Epochs":1}}' -p /$DEVICE:0 &
+python SingleExec_v4.py -f CTFP20_SFsH.json -c '{"RunName":"CTF1v0_SFsH_256_'$1'_1_PPO_RN","LoadName":"CTF1v0_SFs_256_'$1'","TotalSamples":256,"Selection":"Random","NumOptions":24,
+    "NetworkHPs":{"EntropyBeta":0.1,"LR Actor":0.00005,"LR Entropy":0.0000001,"BatchSize":256,"MinibatchSize":32,"CriticBeta":0.5,"Gamma":0.98,"lambda":0.98,"Epochs":1}}' -p /$DEVICE:0 &
+python SingleExec_v4.py -f CTFP20_SFsH.json -c '{"RunName":"CTF1v0_SFsH_256_'$1'_1_PPO_HC","LoadName":"CTF1v0_SFs_256_'$1'","TotalSamples":256,"Selection":"Hull_cluster","NumOptions":24,
+    "NetworkHPs":{"EntropyBeta":0.1,"LR Actor":0.00005,"LR Entropy":0.0000001,"BatchSize":256,"MinibatchSize":32,"CriticBeta":0.5,"Gamma":0.98,"lambda":0.98,"Epochs":1}}' -p /$DEVICE:0 &
+wait
